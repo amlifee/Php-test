45d925a7acd73352d93f6024360db57ffa82055e
amlifee <iridenumber181@gmail.com>
2013-10-09 15:22:25 -0700

    Added cleaner browser detection

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+
+
+
+
 <?php
-	echo "test"
+	echo 'Login page under development';
+	/*$host="localhost";
+	$username = "";
+	$password = "";
+	$db_name="test";
+	$tbl_name = "members";
+
+	mysql_connect("$host", "$username", "$password");
+	mysql_select_db("$db_name");*/
 ?>
+
+<html>
+<title> Screwing with PHP </title>
+	<body>
+		<p>
+			<?php
+			echo $_SERVER['HTTP_USER_AGENT'];
+			?>
+		</p>
+	</body>
+	</html>
+
